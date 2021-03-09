8089fec1dcda15ab8caa1eda02c55c23b07ad30e
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-09 17:05:32 +0200

    Update 3(4).js

--- hm3/3(4).js
+++ hm3/3(4).js
@@ -4,9 +4,17 @@
 //     }
 // }
 
-  for (let i = 1; i < 15; i++){
-    let s = (i + i);
-    console.log (s)
-}
+// for (let i = 1; i < 15; i++){
+//     let s = (i + i);
+//     console.log (s)
+// }
   
 //Создал новую переменную
+
+let sum = 0;
+for (let i = 0; i <= 15; i++){
+    sum += i;
+}
+console.log (sum)
+
+// выходит одним числом. Так должно быть , как я понял
