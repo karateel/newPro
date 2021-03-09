2e89c62cdbdcec66e04e77137bb8136808603f46
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-09 17:48:09 +0200

    Update 4(4).js

--- hm4/4(4).js
+++ hm4/4(4).js
@@ -1,8 +1,30 @@
-function calc(x, y, action) {
-    x = +prompt ("Number one")
-    y = +prompt ("Number two")
-    action = prompt ('+ , - , * , / , % , ^')
-    console.log(x,y,action);   
+// function calc(x, y, action) {
+//     x = +prompt ("Number one")
+//     y = +prompt ("Number two")
+//     action = prompt ('+ , - , * , / , % , ^')
+//     console.log(x,y,action);   
+//     switch (action){
+//         case "+":
+//             return x + y;
+//         case "-":
+//             return x - y;
+//         case "*":
+//             return x * y;
+//         case "/":
+//             return x / y;
+//         case "%":
+//             return x % y;
+//         case "^":
+//             return x ** y;
+//         default:
+//             alert ("Error")
+//             break;
+//     }
+//     return x,y,action;
+// }
+// console.log(calc(''))
+
+function calc(x, y, action) {   
     switch (action){
         case "+":
             return x + y;
@@ -17,9 +39,9 @@ function calc(x, y, action) {
         case "^":
             return x ** y;
         default:
-            alert ("Error")
             break;
     }
     return x,y,action;
 }
-console.log(calc(''))
+const calcSearched = calc (6, 8, "*");
+console.log (calcSearched);
