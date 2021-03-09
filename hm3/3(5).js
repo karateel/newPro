740aa7c1ec0e4b6194baa7cf9b3f1037fda0c635
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-09 17:16:54 +0200

    Update 3(5).js

--- hm3/3(5).js
+++ hm3/3(5).js
@@ -5,9 +5,15 @@
 //     }
 
 
-for (let s = 15; s <= 35; s++) {
-    let d = (s * s);
-    console.log(d)
-}
+// for (let s = 15; s <= 35; s++) {
+//     let d = (s * s);
+//     console.log(d)
+// }
 
 //Переписал на 1 переменную
+
+let multiplication = 1;
+for (let i = 15; i <= 35; i++){
+    multiplication *= i;
+}
+console.log (multiplication)
