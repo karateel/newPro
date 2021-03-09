c5d90e7b2fd3dcf153d69ee77fc5859d07a76f56
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-09 17:41:30 +0200

    Update 4(1).js

--- hm4/4(1).js
+++ hm4/4(1).js
@@ -6,13 +6,20 @@
 // alert (square);
 
 
-function calcCirlce (){
-    let r = +prompt ("Введите радиус")
-    let d = ((r**2) * Math.PI);
-    console.log ("square:" , d) 
-    return d;  
-}
-calcCirlce('')
+// function calcCirlce (){
+//     let r = +prompt ("Введите радиус")
+//     let d = ((r**2) * Math.PI);
+//     console.log ("square:" , d) 
+//     return d;  
+// }
+// calcCirlce('')
 
 
 //переписал так (версия 1.1)
+
+function calcCirlce (x){
+    return ((x**2) * Math.PI); 
+}
+
+let calcCricleArea = calcCirlce (3);
+console.log (calcCricleArea);
