31ee010ad1b52fe05a8d1704ccc305bf8d0fb44e
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-12 16:21:45 +0200

    Update 3(11).js

--- hm3/3(11).js
+++ hm3/3(11).js
@@ -13,19 +13,35 @@
 //     console.log (n)
 // }
 
-let n = Math.floor(Math.random() * 10) + 1;
-while(true){
-    let s = prompt ("number?")
-    if (s === '' || s === null){
-        break;  
-    } else if (isNaN(n)) {
-        alert ("Its not a number!")
-        break;
-    }else if (s < n || s > n){
-        confirm ("try again")
-    } else {
-        alert ("Well Played")
+// let n = Math.floor(Math.random() * 10) + 1;
+// while(true){
+//     let s = prompt ("number?")
+//     if (s === '' || s === null){
+//         break;  
+//     } else if (isNaN(n)) {
+//         alert ("Its not a number!")
+//         break;
+//     }else if (s < n || s > n){
+//         confirm ("try again")
+//     } else {
+//         alert ("Well Played")
+//         break;
+//     }
+//     console.log (n)
+// }
+
+
+const n = Math.floor(Math.random() * 10) + 1;
+console.log (n);
+let s;
+while(s !== n){
+    let s = +prompt ("number?");
+    if (s === n){
+        alert ("ok");
         break;
     }
-    console.log (n)
+    if (s <= null){
+        alert ("kk");
+        break
+    }
 }
