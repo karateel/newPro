9387bca62301031e4b2f62dd0401f02964185f06
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-21 17:55:50 +0200

    Update forEach.js

--- hw6/forEach.js
+++ hw6/forEach.js
@@ -1,8 +1,18 @@
+// const m = [1, 2, 3];
+// function numberSearch(value){
+//     console.log ("numberSearch", value);
+// }
+
+
+// const d = m.forEach(numberSearch);
+// console.log (m);
+
+
 const m = [1, 2, 3];
-function numberSearch(value){
-    console.log ("numberSearch", value);
+
+function callback(value) {
+  return value;
 }
 
-
-const d = m.forEach(numberSearch);
-console.log (m);
+const fakeEach = callback (m);
+console.log (m, fakeEach);
