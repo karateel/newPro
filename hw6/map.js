666da23000ce9d23c23cec2c866ea464996c026d
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-21 17:38:26 +0200

    Update map.js

--- hw6/map.js
+++ hw6/map.js
@@ -1,3 +1,4 @@
+  
 // const numbers = [1, 3, 3, 7]
 // function testingMap(value){
 //     if (value % 3 === 0){
@@ -9,10 +10,17 @@
 // const y = numbers.map(testingMap);
 // console.log (y)
 
-const numbers = [16, 13, 22, 34, 18];
-let newNumbers = [];
- for (let i = 0; i < numbers.length; i++){
-   newNumbers.push(numbers[i] / 2)
- };
-console.log (numbers , newNumbers);
-// 
+const numberArray = [13, 18, 22, 80, 15]
+
+function selfMap(array) {
+  let fakeArray = [];
+  for (let i = 0; i < array.length; i++) {
+    fakeArray.push(array[i] * 2);
+  }
+  return fakeArray;
+}
+const mapTest = selfMap(numberArray)
+console.log (mapTest)
+
+
+
