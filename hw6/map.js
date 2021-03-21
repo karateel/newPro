13abe0e1ffe20926e6357e42b2ca0401b9eb7525
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-21 16:57:07 +0200

    Update map.js

--- hw6/map.js
+++ hw6/map.js
@@ -1,11 +1,18 @@
-const numbers = [1, 3, 3, 7]
-function testingMap(value){
-    if (value % 3 === 0){
-        return (true);
-    } else {
-        return (false);
-    }
-}
-const y = numbers.map(testingMap);
-console.log (y)
+// const numbers = [1, 3, 3, 7]
+// function testingMap(value){
+//     if (value % 3 === 0){
+//         return (true);
+//     } else {
+//         return (false);
+//     }
+// }
+// const y = numbers.map(testingMap);
+// console.log (y)
 
+const numbers = [16, 13, 22, 34, 18];
+let newNumbers = [];
+ for (let i = 0; i < numbers.length; i++){
+   newNumbers.push(numbers[i] / 2)
+ };
+console.log (numbers , newNumbers);
+// 
