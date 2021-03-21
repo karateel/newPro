739fcac65bd80be1f4d039e875eb6e3fb22326fe
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-21 17:48:45 +0200

    Update filter.js

--- hw6/filter.js
+++ hw6/filter.js
@@ -1,14 +1,39 @@
-const salary = [
-    {proffesion : "IT", salary : 15000},
-    {proffesion : "lawyer", salary : 12000},
-    {proffesion : "translater", salary : 16000},
-    {proffesion : "sailor", salary : 240000},
-    {proffesion : "economist", salary : 11000},
-    {proffesion : "architect", salary : 15000},
-    {proffesion : "teacher", salary : 6500},
-    {proffesion : "medician", salary : 7300},
+// const salary = [ //   {proffesion : "IT", salary : 15000},
+//   {proffesion : "lawyer", salary : 12000},
+//   {proffesion : "translater", salary : 16000},
+//   {proffesion : "sailor", salary : 240000},
+//   {proffesion : "economist", salary : 11000},
+//   {proffesion : "architect", salary : 15000},
+//   {proffesion : "teacher", salary : 6500},
+//   {proffesion : "medician", salary : 7300},
+// ];
+// const bigSalay = salary.filter(function (m){
+//   return m.salary > 13000
+// })
+// console.log (bigSalay);
+
+
+
+const salary = [   
+  {proffesion : "IT", salary : 15000},
+  {proffesion : "lawyer", salary : 12000},
+  {proffesion : "translater", salary : 16000},
+  {proffesion : "sailor", salary : 240000},
+  {proffesion : "economist", salary : 11000},
+  {proffesion : "architect", salary : 15000},
+  {proffesion : "teacher", salary : 6500},
+  {proffesion : "medician", salary : 7300},
 ];
-const bigSalay = salary.filter(function (m){
-    return m.salary > 13000
-})
-console.log (bigSalay);
+
+function selfFilter(array) {
+  let fakeArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].salary >= 10000) {
+      fakeArray.push(array[i]);
+    }
+  }
+  return fakeArray;
+}
+
+const fakeFilter = selfFilter(salary)
+console.log (salary, fakeFilter)
