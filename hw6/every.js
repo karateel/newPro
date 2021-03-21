4fd686440507a349a2c24edc9a9aad82377cc3f1
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-21 18:03:54 +0200

    Update every.js

--- hw6/every.js
+++ hw6/every.js
@@ -1,11 +1,29 @@
-function lookingFor (value){
-    return value > 10;
+// function lookingFor (value){
+//   return value > 10;
+// }
+
+// const z = [19, 3, 18, 42, 9];
+// const l = [44, 13, 19, 22, 1337];
+
+// const r = z.every(lookingFor);
+// const r1 = l.every(lookingFor);
+
+// console.log (r, r1)
+
+
+
+
+const numbers = [18, 5, 13, 22, 3]
+
+function fakEvery(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= 10) {
+      return false;
+    } else {
+    return true;
+  }
+ }
 }
 
-const z = [19, 3, 18, 42, 9];
-const l = [44, 13, 19, 22, 1337];
-
-const r = z.every(lookingFor);
-const r1 = l.every(lookingFor);
-
-console.log (r, r1)
+const fakeNumbers = fakEvery (numbers);
+console.log (fakeNumbers)
