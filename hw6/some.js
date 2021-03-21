5434e9923e1bf206183f64572112d884aa800732
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-21 18:08:35 +0200

    Update some.js

--- hw6/some.js
+++ hw6/some.js
@@ -1,9 +1,22 @@
+// const moreNumbers = [17 , 90 , 3, 18 , 4 , 9];
+// const numbersCheck = moreNumbers.some (function (z){
+//     if (z <= 0){
+//         return (true);
+//     } else {
+//         return (false);
+//     }
+// });
+// console.log (numbersCheck)
+
 const moreNumbers = [17 , 90 , 3, 18 , 4 , 9];
-const numbersCheck = moreNumbers.some (function (z){
-    if (z <= 0){
-        return (true);
+function fakeSome (array){
+  for (let i = 0; i < array.length; i++){
+    if (array[i] <= 0){
+      return true
     } else {
-        return (false);
+      return false
     }
-});
-console.log (numbersCheck)
+  }
+}
+const fakeNumbers = fakeSome(moreNumbers);
+console.log (moreNumbers, fakeNumbers)
