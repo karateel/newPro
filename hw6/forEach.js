71f6b8a27fbfa94342a4c7a07e9eac4de5fe8cef
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-23 14:53:11 +0200

    Update forEach.js

--- hw6/forEach.js
+++ hw6/forEach.js
@@ -8,11 +8,26 @@
 // console.log (m);
 
 
-const m = [1, 2, 3];
+// const m = [1, 2, 3];
 
-function callback(value) {
-  return value;
+// function callback(value) {
+//   return value;
+// }
+
+// const fakeEach = callback (m);
+// console.log (m, fakeEach);
+
+
+const lastChanse = [1, 2, 3, 4, 5, -9, -10];
+
+function fakeForEach(array, callBack) {
+    for (let value of array) {
+        callBack(value);
+    }
 }
+function fakeEach(value) {
+    console.log(value);
+}
+fakeForEach(lastChanse, fakeEach);
 
-const fakeEach = callback (m);
-console.log (m, fakeEach);
+
