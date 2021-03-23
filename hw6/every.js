e6a7c83d56276b92eca5fefc161a488cb3488557
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-23 15:17:04 +0200

    Update every.js

--- hw6/every.js
+++ hw6/every.js
@@ -13,17 +13,39 @@
 
 
 
-const numbers = [18, 5, 13, 22, 3]
+// const numbers = [18, 5, 13, 22, 3]
 
-function fakEvery(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] >= 10) {
-      return false;
-    } else {
-    return true;
-  }
- }
+// function fakEvery(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] >= 10) {
+//       return false;
+//     } else {
+//     return true;
+//   }
+//  }
+// }
+
+// const fakeNumbers = fakEvery (numbers);
+// console.log (fakeNumbers)
+
+
+const lastChanse = [1, 2, 3, 4, 5, -9, -10];
+
+function fakeEvery (array, callBack){
+    for (let value of array){
+        if(!callBack(value)){
+            return false
+        }
+        return true
+    }
 }
 
-const fakeNumbers = fakEvery (numbers);
-console.log (fakeNumbers)
+function fakeEveryMath (value){
+    return value % 2 === 0
+}
+
+const fakeEveryLog = fakeEvery(lastChanse, fakeEveryMath);
+console.log (fakeEveryLog)
+
+
+
