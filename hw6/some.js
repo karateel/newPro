6c66bce8a6dda63f9982867792a750f40bcceb68
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-23 15:09:35 +0200

    Update some.js

--- hw6/some.js
+++ hw6/some.js
@@ -1,22 +1,43 @@
-// const moreNumbers = [17 , 90 , 3, 18 , 4 , 9];
-// const numbersCheck = moreNumbers.some (function (z){
-//     if (z <= 0){
-//         return (true);
-//     } else {
-//         return (false);
-//     }
-// });
-// console.log (numbersCheck)
+// // const moreNumbers = [17 , 90 , 3, 18 , 4 , 9];
+// // const numbersCheck = moreNumbers.some (function (z){
+// //     if (z <= 0){
+// //         return (true);
+// //     } else {
+// //         return (false);
+// //     }
+// // });
+// // console.log (numbersCheck)
 
-const moreNumbers = [17 , 90 , 3, 18 , 4 , 9];
-function fakeSome (array){
-  for (let i = 0; i < array.length; i++){
-    if (array[i] <= 0){
-      return true
-    } else {
-      return false
+// const moreNumbers = [17 , 90 , 3, 18 , 4 , 9];
+// function fakeSome (array){
+//   for (let i = 0; i < array.length; i++){
+//     if (array[i] <= 0){
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+// }
+// const fakeNumbers = fakeSome(moreNumbers);
+// console.log (moreNumbers, fakeNumbers)
+
+
+const lastChanse = [1, 2, 3, 4, 5, -9, -10];
+
+function fakeSome (array, callBack){
+    const fakeLastChanse = [];
+    for (let value in array){
+        if (callBack(value)){
+            return true
+        }
+        return false
     }
-  }
+    return fakeLastChanse
 }
-const fakeNumbers = fakeSome(moreNumbers);
-console.log (moreNumbers, fakeNumbers)
+
+function fakeSomeMath (value) {
+    return value % 2 === 0
+}
+
+const fakeSomeLog = fakeSome (lastChanse, fakeSomeMath);
+console.log (fakeSomeLog)
