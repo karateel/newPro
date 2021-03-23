f2fd48c6d32bd419f3cc96b8acf9bd968e008791
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-23 14:59:09 +0200

    Update map.js

--- hw6/map.js
+++ hw6/map.js
@@ -1,26 +1,42 @@
   
-// const numbers = [1, 3, 3, 7]
-// function testingMap(value){
-//     if (value % 3 === 0){
-//         return (true);
-//     } else {
-//         return (false);
-//     }
+// // const numbers = [1, 3, 3, 7]
+// // function testingMap(value){
+// //     if (value % 3 === 0){
+// //         return (true);
+// //     } else {
+// //         return (false);
+// //     }
+// // }
+// // const y = numbers.map(testingMap);
+// // console.log (y)
+
+// const numberArray = [13, 18, 22, 80, 15]
+
+// function selfMap(array) {
+//   let fakeArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     fakeArray.push(array[i] * 2);
+//   }
+//   return fakeArray;
 // }
-// const y = numbers.map(testingMap);
-// console.log (y)
+// const mapTest = selfMap(numberArray)
+// console.log (numberArray, mapTest)
 
-const numberArray = [13, 18, 22, 80, 15]
 
-function selfMap(array) {
-  let fakeArray = [];
-  for (let i = 0; i < array.length; i++) {
-    fakeArray.push(array[i] * 2);
-  }
-  return fakeArray;
+
+const lastChanse = [1, 2, 3, 4, 5, -9, -10];
+
+function fakeMap (array, callback){
+    const fakeLastChanse = [];
+    for (let value of array){
+        fakeLastChanse.push(callback(value))
+    }
+    return fakeLastChanse
 }
-const mapTest = selfMap(numberArray)
-console.log (numberArray, mapTest)
 
+function fakeMath (value){
+    return value / 2
+}
 
-
+const fakeMapConsole = fakeMap (lastChanse, fakeMath);
+console.log (fakeMapConsole)
