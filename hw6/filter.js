91ba3a68c7ade7d335bc96810d1602ff34bf7484
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-23 15:04:39 +0200

    Update filter.js

--- hw6/filter.js
+++ hw6/filter.js
@@ -1,4 +1,21 @@
-// const salary = [ //   {proffesion : "IT", salary : 15000},
+// // const salary = [ //   {proffesion : "IT", salary : 15000},
+// //   {proffesion : "lawyer", salary : 12000},
+// //   {proffesion : "translater", salary : 16000},
+// //   {proffesion : "sailor", salary : 240000},
+// //   {proffesion : "economist", salary : 11000},
+// //   {proffesion : "architect", salary : 15000},
+// //   {proffesion : "teacher", salary : 6500},
+// //   {proffesion : "medician", salary : 7300},
+// // ];
+// // const bigSalay = salary.filter(function (m){
+// //   return m.salary > 13000
+// // })
+// // console.log (bigSalay);
+
+
+
+// const salary = [   
+//   {proffesion : "IT", salary : 15000},
 //   {proffesion : "lawyer", salary : 12000},
 //   {proffesion : "translater", salary : 16000},
 //   {proffesion : "sailor", salary : 240000},
@@ -7,33 +24,36 @@
 //   {proffesion : "teacher", salary : 6500},
 //   {proffesion : "medician", salary : 7300},
 // ];
-// const bigSalay = salary.filter(function (m){
-//   return m.salary > 13000
-// })
-// console.log (bigSalay);
+
+// function selfFilter(array) {
+//   let fakeArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i].salary >= 10000) {
+//       fakeArray.push(array[i]);
+//     }
+//   }
+//   return fakeArray;
+// }
+
+// const fakeFilter = selfFilter(salary)
+// console.log (salary, fakeFilter)
 
 
+const lastChanse = [1, 2, 3, 4, 5, -9, -10];
 
-const salary = [   
-  {proffesion : "IT", salary : 15000},
-  {proffesion : "lawyer", salary : 12000},
-  {proffesion : "translater", salary : 16000},
-  {proffesion : "sailor", salary : 240000},
-  {proffesion : "economist", salary : 11000},
-  {proffesion : "architect", salary : 15000},
-  {proffesion : "teacher", salary : 6500},
-  {proffesion : "medician", salary : 7300},
-];
-
-function selfFilter(array) {
-  let fakeArray = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].salary >= 10000) {
-      fakeArray.push(array[i]);
+function fakeFilter (array, callBack){
+    const fakeLastChanse = [];
+    for (let value in array){
+        if (callBack(value)){
+            fakeLastChanse.push(value)
+        }
     }
-  }
-  return fakeArray;
+    return fakeLastChanse
 }
 
-const fakeFilter = selfFilter(salary)
-console.log (salary, fakeFilter)
+function fakeFilterMath (value){
+    return value > 3
+}
+
+const fakeFilterLog = fakeFilter(lastChanse,fakeFilterMath);
+console.log (fakeFilterLog)
