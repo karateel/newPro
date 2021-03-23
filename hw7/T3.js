86cdb7fbe555568059a2ce7bb65cd9c11a4042cb
kyurov <78860828+karateel@users.noreply.github.com>
2021-03-23 15:38:51 +0200

    Create T3.js

---
+++ hw7/T3.js
@@ -0,0 +1,19 @@
+const originalObj = {
+    name: Tony,
+    lastname: Stark
+};
+
+function copy (target, origin){
+    for (let key in origin){
+        target[key] = origin[key];
+    }
+    return target
+}
+
+
+const copyObj = copy ({}, originalObj);
+console.log (copyObj)
+
+
+
+
