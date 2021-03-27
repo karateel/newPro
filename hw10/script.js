2defe69095803d2a446c88ce61da44dcfd386e57
kyurov <karateel@users.noreply.github.com>
2021-03-27 18:45:25 +0200

    10hw

--- hw10/script.js
+++ hw10/script.js
@@ -1,22 +1,26 @@
-"use strict"
+const numberArray = [1, 2, 3, 4, 5];
 
-function askPassword(ok, fail) {
-    let password = prompt("Password?", '');
-    if (password == "rockstar") ok();
-    else fail();
-  }
-  
-  let user = {
-    name: 'Вася',
-  
-    loginOk() {
-      alert(`${this.name} logged in`);
-    },
-  
-    loginFail() {
-      alert(`${this.name} failed to log in`);
-    },
-  
-  };
-  
-  askPassword(user.loginOk, user.loginFail);
+const numberArrayPlus = [1, 2, 3, [4, 5], 6];
+
+
+function listMade(array){
+    let ul = document.createElement("ul");
+        for (let value of array){
+            if (Array.isArray(value)){
+                ul.appendChild(listMade(value));
+            } else {
+                let li = document.createElement("li");
+                li.innerText = value;
+                ul.appendChild(li);
+            }
+        }
+    return ul;
+};
+
+const makingList = listMade (numberArrayPlus);
+document.body.appendChild(makingList);
+
+
+
+
+
